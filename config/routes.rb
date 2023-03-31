2701745eb2633fcd2192f88349ec1f6d275422d9
Matthew Kolarik <ejkmdk@gmail.com>
2023-03-31 01:34:25 +0000

    Added in scaffolding

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
+  resources :projects
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  # defines root path route
+  #root
+  root "projects#index"
+  resources :projects
 end
