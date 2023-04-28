3c8e6d8903bd90a960d77512fdd76943f55dd3da
Matthew Kolarik <ejkmdk@gmail.com>
2023-04-28 06:49:33 +0000

    Done

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,5 @@
 class ApplicationController < ActionController::Base
 
-    before_action :set_project, only: %i[ show edit update destroy ]
-  before_action :authenticate_user!, only: %i[ new edit create update destroy]
-  
+before_action :authenticate_user!, only: %i[ new edit create update destroy]
+
 end
